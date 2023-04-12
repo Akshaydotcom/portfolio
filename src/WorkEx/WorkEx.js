79bac8aa268ad7ca9fadec88b8d6d052f805c4a4
AkshayGB <akshaybhandaryg1@gmail.com>
2023-04-12 14:38:13 -0700

    Removed Warnings

--- src/WorkEx/WorkEx.js
+++ src/WorkEx/WorkEx.js
@@ -2,20 +2,20 @@ import React from 'react';
 import Infosys from './Infosys_logo.svg.png'
 import FIH from './FIH_logo.png'
 import UBS from './UBS_Logo.svg'
-import {Work} from './Work';
+// import {Work} from './Work';
 import './WorkEx.css'
 export const WorkEx=()=>{
-    let jd1="Designed and implemented a dashboard using React, TypeScript and Redux to create different investment portfolios for a major global financial firm on basis of income and risk levels. Integrated multiple Java Based proprietary REST APIs with dashboard to persist data between different steps of the web app. Implemented automation test cases using Jest to test different components of the web app that reduced time spent in testing by as much as 30%"
-    const [windowOpen, setWindow] = React.useState(false);
-    const openWindow = () => {
-        windowOpen ? setWindow(false) : setWindow(true);
-      };
+    // let jd1="Designed and implemented a dashboard using React, TypeScript and Redux to create different investment portfolios for a major global financial firm on basis of income and risk levels. Integrated multiple Java Based proprietary REST APIs with dashboard to persist data between different steps of the web app. Implemented automation test cases using Jest to test different components of the web app that reduced time spent in testing by as much as 30%"
+    // const [windowOpen, setWindow] = React.useState(false);
+    // const openWindow = () => {
+    //     windowOpen ? setWindow(false) : setWindow(true);
+    //   };
     return(<div id="Work" className='main_workExp'>
         <h2>Places I have worked</h2>
         <div>
             
             <img alt="" src={Infosys} className='bg-white' style={{"width":"37%"}}/>
-            {windowOpen && <Work />}
+            {/* {windowOpen && <Work />} */}
             <img alt="" src={FIH}  className='bg-white' style={{"width":"33%"}}/>
             <img alt="" src={UBS}  className='bg-white' style={{"width":"70%"}}/>
             
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import './Navbar.css'
 import pdf from './Resume.pdf'
 export const Navbar=()=>{
