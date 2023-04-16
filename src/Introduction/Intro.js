ca97e548bd7da755501c5b0133a762b9c6781fea
AkshayGB <akshaybhandaryg1@gmail.com>
2023-04-15 23:15:23 -0700

    Added hamburger menu for mobile devices for navbar

--- src/Introduction/Intro.js
+++ src/Introduction/Intro.js
@@ -1,6 +1,6 @@
 import './Intro.css'
 export const Intro=()=>{
-    return(<div className='Intro'>
+    return(<div className='Intro margin-top-mobile'>
         <p className="first">Hi, I'm</p>
         <p className="full-name"> Akshay Bhandary.</p>
         <p className="job-profile"> I create great digital experiences.</p>
