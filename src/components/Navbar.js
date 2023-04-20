8961fd0af2ff30f945161aa2f759ee3aa45f4d09
AkshayGB <akshaybhandaryg1@gmail.com>
2023-04-20 13:29:08 -0700

    Implemented changes for responsive design

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -28,13 +28,15 @@ export const Navbar=()=>{
                     {!menu && <svg className="hamburger-menu" viewBox="0 0 1024 1024" fill="#ffffff" stroke="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path fill="#ffffff" d="M195.2 195.2a64 64 0 0 1 90.496 0L512 421.504 738.304 195.2a64 64 0 0 1 90.496 90.496L602.496 512 828.8 738.304a64 64 0 0 1-90.496 90.496L512 602.496 285.696 828.8a64 64 0 0 1-90.496-90.496L421.504 512 195.2 285.696a64 64 0 0 1 0-90.496z"></path></g></svg>}
                 </div>
             }
-            <ul className={navbarType==='mobile'&&menu?"navbar-list hide":"navbar-list show"}>
-                <li><a href="#AboutMe">About Me</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><a href="#Work">Work Experience</a></li>
-                <li><a href="#ContactMe">Contact Me</a></li>
-                <li><a href={pdf} download="Resume_Akshay">Resume</a></li>
-        </ul>
+            <div className={navbarType==='mobile'&&menu?"hidediv":"showdiv"}>
+                <ul className={navbarType==='mobile'&&menu?"navbar-list hide":"navbar-list show"}>
+                    <li><a href="#AboutMe">About Me</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#Work">Work Experience</a></li>
+                    <li><a href="#ContactMe">Contact Me</a></li>
+                    <li><a href={pdf} download="Resume_Akshay">Resume</a></li>
+                </ul>
+            </div>
         <div className='scroll-progress'></div>
         </div>
     )
