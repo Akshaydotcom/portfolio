8c48e3760461951bf652ea345fa0c276cbbc9563
AkshayGB <akshaybhandaryg1@gmail.com>
2023-08-09 16:56:49 -0700

    Implemented Suggestions

--- src/App.js
+++ src/App.js
@@ -8,19 +8,29 @@ import { Intro } from './Introduction/Intro';
 import {Projects} from './Projects/Projects';
 import { ContactMe } from './contactMe/ContactMe';
 import { WorkEx } from './WorkEx/WorkEx';
-import React from 'react';
+import React, {useEffect, useRef} from 'react';
 function App() {
   const particlesInit = async (main) =>{
     await loadFull(main);
   }
+
+  const ref=useRef(null)
+
+  useEffect(()=>{
+    setTimeout(()=>{
+      ref.current?.scrollIntoView({behavior:"smooth"});
+    },[3000])
+  },[])
+
   return (
     <div className="App">
       <Navbar /> 
       <section><Intro /></section>
-      <section><AboutMe/></section>
+      <section ref={ref}><AboutMe/></section>
       <section><WorkEx /></section>
       <section><Projects /></section>
       <section><ContactMe /></section>
+      <footer style={{"color":"white","position":"sticky","textAlign":"center"}}>Made By Akshay Bhandary</footer>
       <Particles id="particles-here" init={particlesInit} options={particlesconfig}/>
     </div>
   );
